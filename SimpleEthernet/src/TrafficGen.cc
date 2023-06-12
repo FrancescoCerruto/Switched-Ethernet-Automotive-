2ef233ff2aed4b3938a614f58aad023987ccaf49
Francesco Cerruto <1000005927studium.unict.it>
2023-06-12 15:17:40 +0200

    fix RC payload, burst size and period

--- SimpleEthernet/src/TrafficGen.cc
+++ SimpleEthernet/src/TrafficGen.cc
@@ -91,6 +91,8 @@ void TrafficGen::generate() {
 
         pkt->setControlInfo(ci);
         send(pkt, "lowerLayerOut");
+
+        EV_INFO << "Sono nel modulo " << getParentModule()->getFullName() << " " << getFullName() << "e sto generando la frame n. " << TxSeqno << endl;
     }
 }
 
